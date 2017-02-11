4803465cb40b14e0ac7542bd6c9eeacd30f06f5a
Berkus Decker <berkus@madfire.net>
2017-02-11 16:38:00 +0200

    Fix include path

--- Telegram/SourceFiles/codegen/style/parsed_file.h
+++ Telegram/SourceFiles/codegen/style/parsed_file.h
@@ -23,7 +23,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include <memory>
 #include <string>
 #include <functional>
-#include <QImage>
+#include <QtGui/QImage>
 #include "codegen/common/basic_tokenized_file.h"
 #include "codegen/style/options.h"
 #include "codegen/style/module.h"
