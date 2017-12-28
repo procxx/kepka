5eec3aa1580341ab677924be7f07dccc4b67dce4
Berkus Decker <berkus@madfire.net>
2017-12-28 21:12:25 +0200

    Add clutches for conan-limp

--- .travis/os_build.sh
+++ .travis/os_build.sh
@@ -3,11 +3,14 @@
 set -x
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    docker run --rm -v $PWD:/repo -v ~/.travis:/travis berkus/docker-cpp-ci /bin/sh -c "cd /repo/_build_; /repo/.travis/build.sh"
+    docker run --rm -v $PWD:/repo -v ~/.travis:/travis berkus/docker-cpp-ci /bin/sh -c "cd /repo/_build_; conan install .. --build missing; /repo/.travis/build.sh"
 fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     cd _build_
+    export CC=clang
+    export CXX=clang++
     export EXTRA_CMAKE_FLAGS=-DCMAKE_PREFIX_PATH='/usr/local/opt/qt5/;/usr/local/opt/openal-soft'
+    conan install -s compiler=apple-clang .. --build missing
     ../.travis/build.sh
 fi
