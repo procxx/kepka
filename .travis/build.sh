9c55a71a5e3a200a84950c35f770d1598398b799
Berkus Decker <berkus@madfire.net>
2017-12-28 02:42:44 +0200

    Add OSX build

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /repo/_build_
-cmake -G Ninja -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTING=ON .. || exit 1
-cmake --build . || exit 1
-ASAN_OPTIONS=alloc_dealloc_mismatch=0 ctest || exit 1
+set -x
+
+conan install .. --build missing
+cmake -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTING=ON .. || exit 1
+cmake --build . -- -v || exit 1
+ASAN_OPTIONS=alloc_dealloc_mismatch=0 ctest . || exit 1
