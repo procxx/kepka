bbb718dcca0b8fbcf910b5f323e9255ab2de5f22
Berkus Decker <berkus@madfire.net>
2017-12-28 21:12:25 +0200

    Add clutches for conan-limp

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,6 @@
 
 set -x
 
-conan install .. --build missing
 cmake -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE $EXTRA_CMAKE_FLAGS -DBUILD_TESTING=ON .. || exit 1
 cmake --build . -- -v || exit 1
 ASAN_OPTIONS=alloc_dealloc_mismatch=0 ctest . || exit 1
