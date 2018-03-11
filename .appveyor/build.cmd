b2b6c5bd25590f0422c64bb76b713ee2aedfa6d7
Friedrich von Never <friedrich@fornever.me>
2018-03-11 11:22:29 +0700

    Run ctest on appveyor (#57)

--- .appveyor/build.cmd
+++ .appveyor/build.cmd
@@ -1,4 +1,5 @@
 pushd %APPVEYOR_BUILD_FOLDER%\build
 cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 cmake --build . --config RelWithDebInfo
+ctest .
 popd
