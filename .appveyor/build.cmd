de6b51bc4e43cc0d1e52cef51b1a18d7b47a52aa
Friedrich von Never <friedrich@fornever.me>
2018-10-27 22:02:09 +0700

    Convert build script to PowerShell
    Close #205.

--- .appveyor/build.cmd
+++
@@ -1,5 +0,0 @@
-pushd %APPVEYOR_BUILD_FOLDER%\build
-cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-cmake --build . --config RelWithDebInfo
-ctest .
-popd
