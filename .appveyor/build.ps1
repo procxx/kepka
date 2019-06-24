7633c8252bb834e18f8e27d547c8214023aacd2c
Friedrich von Never <friedrich@fornever.me>
2019-06-24 22:27:45 +0700

    Send stderr from cmake into stdout
    This will no more break build when warnings are received from cmake.

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -1,11 +1,21 @@
 $ErrorActionPreference = 'Stop'
 
 Push-Location "$env:APPVEYOR_BUILD_FOLDER\build"
+
+function execute-cmake {
+    # We have to merge the stderr and stdout here because otherwise the build will fail on any random warning
+    cmd /c 'cmake 2>&1' @args
+    if ($LASTEXITCODE -ne 0) {
+        throw "CMake execution error: $LASTEXITCODE"
+    }
+}
+
 try {
-    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-    if (!$?) { throw 'Error running cmake' }
-    cmake --build . --config RelWithDebInfo
-    if (!$?) { throw 'Error building with cmake' }
+    execute-cmake '-GNinja' `
+        '-DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake' `
+        '-DCMAKE_BUILD_TYPE=RelWithDebInfo' `
+        ..
+    execute-cmake --build . --config RelWithDebInfo
     ctest .
     if (!$?) { throw 'ctest execution error' }
 } finally {
